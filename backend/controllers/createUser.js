5b9d6d3aa24b5863579a544d5d29588d5f331c1c
Pratik Parida <paridapratik709@gmail.com>
2025-02-17 21:08:14 +0530

    Authentication

--- backend/controllers/createUser.js
+++ backend/controllers/createUser.js
@@ -1,6 +1,27 @@
 import mongoose from "mongoose";
 import User from "../models/userModel.js";
-const createUser=async(req,res)=>{
+import bcrypt from "bcryptjs";
+const createUser=async(req,res)=>
+{
+    const {username,email,password}=req.body;
+    if(!username || !email ||!password )
+    {
+        res.status(400).send({message:"Please Fill All The Inputs Yar"});
+    }
+    const userMail=await User.findOne({email});
+    if(userMail)
+    {
+        res.status(401).send({message:"This Mail Id Already Registered"});
+    }
+    else
+    {
+        const salt=await bcrypt.genSalt(10);
+        const hashPassword=await bcrypt.hash(password,salt);
+        const newUser=new User({username,email,password:hashPassword});
+        await newUser.save();
+        res.status(201).send("User Registered Successfully");
+    }
+
 };
 
 export default createUser;
